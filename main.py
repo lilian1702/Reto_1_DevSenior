c614272b68ba9a9239908675409c649c5b1abf7d
lilian1702 <163610265+lilian1702@users.noreply.github.com>
2024-11-16 16:22:31 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import datetime
+
+listaDeExperimentos = [
+    ["experimento1", "16/11/2024","quimica", [5,4,3,2,1]],
+]
+
+
+
+def agregar_experimento():
+    # permite agregar experimentos con sus nuevos atributos
+    pass
+
+def eliminar_experimeintos():
+    #permite eliminar un experimento
+    #dificultad 1: requiere la funcion agregar experimento
+    pass
+
+def visualizar_experimentos():
+    #perimite visualizar todos los experimentos
+    #requiere el uso de la funcion agregar experimeinto. dificultad 1
+    pass
+
+def calcular_estadisticas():
+    # calcular estadisticas basicas (promedios, maximos y minimos de un experimento)
+   #dificultad 2. requiere el uso de funciones agregar_experimeentos
+    
+    pass
+
+def compara_experimentos():
+    #compara dos o mas experimentos para determinar los mejores o peores resultados
+    #dificultad 3, requiere el uso de funciones calcular_funciones
+    pass
+
+def generar_informe():
+    #gererar un infoeme resumen de los experimentos y sus estadisticas. difiultad 3
+    #requiere el suso de funciones visualizar_experimentos y calcular_estadisticas
+    pass
+
+def mostrar_menu():
+    #muestra el menu principal del programa. difiultad 1
+    print("====menu principal=====")
+    print("===gestios de experimentos===")
+    print("1. Agregar experimento")
+    print("2. Visualizar experimentos")
+    print("3. eliminar experimentos")
+    print("====analisis de datos=====")
+    print("4.calcular estadisticas")
+    print("5. comparar experimentos")
+    print("====generar informe=====")
+    print("===salir===")
+    print("Ingrese una opcion:")
+    print ("7. salir")
+
+
+def main():
+    #esta funcion controla el flujo general del sistema
+    pass
+main()
+
+
